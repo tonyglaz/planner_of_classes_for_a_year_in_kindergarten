4e65fee2425187d4ba1ddedccce304ca44c3fdfd
tonyglaz <tonyglazunov@gmail.com>
2022-07-27 21:12:20 +0300

    Добавлены таблицы Child_info и Parent_info Добавлено ограничение доступа неавторизованным пользователем Добавлены Профили пользователей В процессе реализация возможности CRUD таблиц авторизованными пользователями

---
+++ planner/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Child_Info, Parent_Info
+
+admin.site.register(Child_Info)
+admin.site.register(Parent_Info)
--- planner/urls.py
+++ planner/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from .views import (
+    RecordListView,
+    RecordDetailView,
+    RecordDeleteView,
+    RecordCreateView,
+    RecordUpdateView,
+)
 
 urlpatterns = [
-    path('', views.home, name='planner-home'),
-   # path('register/', views.register, name='register')
+    path('', RecordListView.as_view(), name='planner-home'),
+    path('record/new/', RecordCreateView.as_view(), name='record-create'),
+    path('record/<int:pk>/delete/', RecordDeleteView.as_view(), name='record-delete'),
+    path('record/<int:pk>/update/', RecordUpdateView.as_view(), name='record-update'),
+    path('record/<int:pk>/', RecordDetailView.as_view(), name='record-detail'),
+    # path('register/', views.register, name='register')
 ]
